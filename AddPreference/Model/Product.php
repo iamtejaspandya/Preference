9ce486d11d0c3652b29a81236e33c855912059f4
Tejas PANDYA <160587142+iamtejaspandya@users.noreply.github.com>
2024-02-21 15:19:48 +0530

    Add files via upload

---
+++ AddPreference/Model/Product.php
@@ -0,0 +1,9 @@
+<?php
+namespace Tejas\AddPreference\Model;
+
+class Product extends \Magento\Catalog\Model\Product{
+    // public function getName()
+    // {
+    //     return 'Task Done';
+    // }
+}
